12e29025e127bf89d86247ec943b303672e9a7bf
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2020-11-08 13:45:10 -0600

    Finished ex05.py

---
+++ new-code-sprint/ex05.py
@@ -0,0 +1,17 @@
+my_name = 'Brandon'
+my_age = 22
+my_height = 6
+my_weight = 150
+my_eyes = 'bro'
+my_teeth = 'white'
+my_hair = 'bro'
+
+print("Let's talk about %s." % my_name)
+print("He's %d inches tall." % my_height)
+print("He's %d pounds heavy." % my_weight)
+print("Actually thats not too heavy")
+print("Hes got %s eyes and %s hair" % (my_eyes, my_hair))
+print("His teeth are usally %s" % my_teeth)
+
+#this line is tricky
+print("if i add %d, %d, and %d I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight))
