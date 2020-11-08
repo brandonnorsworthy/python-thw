49c2d8f0441eca2b4aa1544130f8a66092196312
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2020-11-08 14:04:20 -0600

    Finished ex09.py

---
+++ new-code-sprint/ex09.py
@@ -0,0 +1,12 @@
+days = 'mon tue wed thu fri sat sun'
+months =  'jan\nfeb\nmar\napr\nmay\njun\njul\naug'
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print( """
+Theres somthing going on  here.
+with the three double quoutes
+
+test test etst test
+""")
