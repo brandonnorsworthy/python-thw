0b7f1bff9f76ca14c9d386f95d6ffc27a6f51b84
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2020-11-08 14:21:09 -0600

    Finished ex12.py

---
+++ new-code-sprint/ex12.py
@@ -0,0 +1,5 @@
+age = input("how old are you")
+height = input("how tall are you")
+weight = input("how much do you weigh")
+
+print("so, your", age, "old, and ", height, "tall and ", weight, "heavy")
