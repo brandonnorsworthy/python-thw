ee5bfae8c8f4606e623dc1fd48c3b4f183de34cf
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2020-11-08 14:25:58 -0600

    Finished ex13.py

---
+++ new-code-sprint/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("script is called", script)
+print("first variable", first)
+print("second variable", second)
+print("third variable", third)
