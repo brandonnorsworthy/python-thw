33e91642d3a6277a572146c199a86fa5a1d2381e
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2020-11-16 11:12:21 -0600

    Finished ex19.py

---
+++ new-code-sprint/ex19.py
@@ -0,0 +1,20 @@
+def chesse_and_crackers(cheese_count, boxes_of_crackers):
+    print("You have", cheese_count, "cheeses!")
+    print("You have", boxes_of_crackers, "boxes of crackers!")
+    print("Man that's enough for a party!")
+    print("Get a blanket.\n")
+
+print("We can just give the function numbers directly:")
+chesse_and_crackers(20,30)
+
+print("OR, we can use variables from out script:")
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+chesse_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print("We can even do math inside too:")
+chesse_and_crackers(10+20, 5+6)
+
+print("And we can combine the two, variables and math")
+chesse_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
